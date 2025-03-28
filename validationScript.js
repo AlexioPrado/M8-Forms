22cd970ae3edc0df2e1a58a28d5eff142b7f41eb
Marcus Alexio Prado <mgprado1@my.waketech.edu>
2025-03-28 13:08:43 -0400

    fixed an error

--- validationScript.js
+++ validationScript.js
@@ -18,10 +18,9 @@ submit.addEventListener('click',verifyAlphaNum)
 function verifyAlphaNum(){
   let user = document.getElementById('inputField')
 
-  //if(!(/[a-z]|[A-Z]$/.test(user))){
   if (user.validity.valueMissing){
     user.setCustomValidity('Please input an alphanumeric value!');
-  } else if ((/[a-z]$/.test(user.value)) || (/[0-9]$/.test(user.value)) || (/[A-Z]$/.test(user.value))){
+  } else if ((/^[a-z0-9A-Z]+$/.test(user.value))){
     user.setCustomValidity('INPUT SUBMITTED!')
   } else {
     user.setCustomValidity('Input is not an alphanumeric value!')
